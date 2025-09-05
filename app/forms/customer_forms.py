8eec71f74df70b80916a11213e9a69c2fcd5e5eb
Shivam-2310 <nshivam2003@gmail.com>
2025-09-05 15:19:35 +0530

    feat: added pep8 compliance to customer forms.py

--- app/forms/customer_forms.py
+++ app/forms/customer_forms.py
@@ -1,15 +1,20 @@
-"""
-CUSTOMER FORMS
-"""
+"""Customer forms."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField, IntegerField, SelectMultipleField, BooleanField, widgets
-from wtforms.validators import DataRequired, Length, Email, NumberRange, Optional
+from wtforms import (
+    BooleanField,
+    IntegerField,
+    SelectField,
+    SelectMultipleField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+    widgets,
+)
+from wtforms.validators import DataRequired, Email, Length, NumberRange, Optional
 
 class ProfileUpdateForm(FlaskForm):
-    """
-    FORM FOR UPDATING CUSTOMER PROFILE
-    """
+    """Form for updating customer profile."""
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=64)])
     name = StringField('Full Name', validators=[DataRequired(), Length(min=2, max=100)])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -18,16 +23,12 @@ class ProfileUpdateForm(FlaskForm):
     submit = SubmitField('UPDATE PROFILE')
 
 class MultiCheckboxField(SelectMultipleField):
-    """
-    CUSTOM FIELD FOR MULTIPLE CHECKBOXES
-    """
+    """Custom field for multiple checkboxes."""
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
 
 class PreferencesForm(FlaskForm):
-    """
-    FORM FOR UPDATING CUSTOMER PREFERENCES
-    """
+    """Form for updating customer preferences."""
     favorite_cuisines = MultiCheckboxField('Favorite Cuisines', choices=[])
     dietary_restrictions = MultiCheckboxField('Dietary Restrictions', choices=[
         ('vegetarian', 'Vegetarian'),
@@ -38,39 +39,45 @@ class PreferencesForm(FlaskForm):
 
 
 class OrderFeedbackForm(FlaskForm):
-    """
-    FORM FOR SUBMITTING ORDER FEEDBACK AFTER COMPLETION
-    """
-    rating = SelectField('Rating', 
-                        choices=[(1, '★'), (2, '★★'), (3, '★★★'), (4, '★★★★'), (5, '★★★★★')],
-                        validators=[DataRequired()],
-                        coerce=int)
+    """Form for submitting order feedback after completion."""
+    rating = SelectField(
+        'Rating',
+        choices=[(1, '★'), (2, '★★'), (3, '★★★'), (4, '★★★★'), (5, '★★★★★')],
+        validators=[DataRequired()],
+        coerce=int,
+    )
     message = TextAreaField('Your Feedback', validators=[DataRequired(), Length(min=10, max=1000)])
     submit = SubmitField('SUBMIT FEEDBACK')
 
 class DishRatingForm(FlaskForm):
-    """
-    FORM FOR RATING INDIVIDUAL DISHES
-    """
+    """Form for rating individual dishes."""
     def __init__(self, order_items=None, *args, **kwargs):
         super(DishRatingForm, self).__init__(*args, **kwargs)
         if order_items:
             for item in order_items:
                 field_name = f'dish_rating_{item.menu_item_id}'
-                setattr(self, field_name, SelectField(
-                    f'Rate {item.menu_item.name}',
-                    choices=[(1, '★'), (2, '★★'), (3, '★★★'), (4, '★★★★'), (5, '★★★★★')],
-                    validators=[DataRequired()],
-                    coerce=int,
-                    default=5
-                ))
+                setattr(
+                    self,
+                    field_name,
+                    SelectField(
+                        f'Rate {item.menu_item.name}',
+                        choices=[
+                            (1, '★'),
+                            (2, '★★'),
+                            (3, '★★★'),
+                            (4, '★★★★'),
+                            (5, '★★★★★'),
+                        ],
+                        validators=[DataRequired()],
+                        coerce=int,
+                        default=5,
+                    ),
+                )
     
     submit = SubmitField('SUBMIT DISH RATINGS')
 
 class SearchForm(FlaskForm):
-    """
-    FORM FOR SEARCHING RESTAURANTS AND MENU ITEMS
-    """
+    """Form for searching restaurants and menu items."""
     query = StringField('Search', validators=[Optional(), Length(max=100)])
     location = StringField('Location', validators=[Optional(), Length(max=100)])
     cuisines = MultiCheckboxField('Cuisines', choices=[])
