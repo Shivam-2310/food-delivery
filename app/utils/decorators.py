2bc0dc3d8f4aed7e9a9001588c4d9cf9d7d9587a
Shivam-2310 <nshivam2003@gmail.com>
2025-09-05 15:06:07 +0530

    feat: added pep8 compliance to decorators.py

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -1,29 +1,23 @@
-"""
-CUSTOM DECORATORS FOR ROUTE AUTHORIZATION
-"""
+"""Custom decorators for route authorization."""
 
 from functools import wraps
 from flask import abort
 from flask_login import current_user
 
 def customer_required(f):
-    """
-    DECORATOR TO RESTRICT ROUTE ACCESS TO CUSTOMERS ONLY
-    """
+    """Restrict route access to customers only."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not current_user.is_customer():
-            abort(403)  # FORBIDDEN
+            abort(403)  # Forbidden.
         return f(*args, **kwargs)
     return decorated_function
 
 def owner_required(f):
-    """
-    DECORATOR TO RESTRICT ROUTE ACCESS TO RESTAURANT OWNERS ONLY
-    """
+    """Restrict route access to restaurant owners only."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not current_user.is_owner():
-            abort(403)  # FORBIDDEN
+            abort(403)  # Forbidden.
         return f(*args, **kwargs)
     return decorated_function
